be354734e16c69821ee433dbff3649a19f314069
Howard Kier <howard@Howards-MacBook-Air.local>
2018-07-05 19:04:28 -0500

    Add Booth Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './Components/NavBar'
+import Booth from './Components/Booth'
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <Booth/>
       </div>
     );
   }
---
+++ src/Components/Booth/Booth.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const Booth = () => {
+    return(
+        <div>
+       
+        <input value="Booth" type="submit"/>  
+        <input type="text"  name="booth" />
+       
+        </div>
+    )
+}
+
+export default Booth;
